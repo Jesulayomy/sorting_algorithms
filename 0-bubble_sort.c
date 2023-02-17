39f91910500ab1005959970b08a5b78dc705425e
Jesulayomy <jesulayomi081@gmail.com>
2023-02-17 13:36:27 +0000

    nullarray

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,26 +12,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - i - 1; j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
 			}
 		}
-		/**
-		 * for (k = 0; k < size; k++)
-		 * {
-		 * printf("%d", array[k]);
-		 * if (k != size - 1)
-		 * printf(", ");
-		 * }
-		 * printf("\n");
-		 */
 	}
 }
